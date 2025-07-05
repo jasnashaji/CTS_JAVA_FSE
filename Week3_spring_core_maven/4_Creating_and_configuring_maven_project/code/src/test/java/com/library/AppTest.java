eae9d025194762b7760f0777192d0038ee55449a
jasnashaji <146013960+jasnashaji@users.noreply.github.com>
2025-07-05 21:42:32 +0530

    Add files via upload

---
+++ Week3_spring_core_maven/4_Creating_and_configuring_maven_project/code/src/test/java/com/library/AppTest.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAppRuns() {
+        assertTrue(true); // basic dummy test
+    }
+}
